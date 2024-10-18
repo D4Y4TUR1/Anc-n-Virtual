012ca11fd7eb3255b9a786f945b8c293c7022893
D4Y4TUR1 <david.tupayachirivas@gmail.com>
2024-10-17 19:46:16 -0500

    feat: install @react-three/fiber and @react-three/drei for 3D rendering support

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://D4Y4TUR1.github.io//AnconVirtual/',
 });
